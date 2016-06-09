94499ce54d56f157c536da4ab0a1ddee2bb4f2f5
Q2key <dimatocky@gmail.com>
2016-06-09 13:08:02 +0400

    initial

---
+++ Npo-2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Npo_2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Matrix m = new Matrix();
+            m.CreateGraf(@"D:\input.txt");
+            Console.ReadKey();
+        }
+
+
+        private class Matrix
+        {
+            private static void Out(List<int> output )
+            {
+                var outputstr = output.Aggregate(string.Empty, (current, t) => current + (t.ToString()+" "));
+                File.WriteAllText(@"D:\output2.txt",outputstr);             
+            }
+
+            public void CreateGraf(string path)
+            {
+                var header =
+                    File.ReadAllLines(path)
+                        .Skip(0)
+                        .First()
+                        .Split(' ')
+                        .Where(s => !string.IsNullOrWhiteSpace(s))
+                        .Select(int.Parse)
+                        .ToArray();
+
+                var edges = header[0];
+                var startp = header[1];
+                var destp = header[2];
+
+                Console.Write(@"Edges count {0}", edges);
+                Console.Write(@" Start point {0}",startp);
+                Console.Write(@" Dest point {0}", destp);
+
+                Console.WriteLine();
+
+                var sarray = File.ReadAllLines(path).Skip(1).ToArray();
+
+                var g = new int[edges][];
+
+                for (var i = 0; i < edges; i++)
+                {
+                    g[i] = sarray[i].Split(' ').
+                    Where(x => !string.IsNullOrWhiteSpace(x)).
+                    Select(int.Parse).ToArray();
+                }
+                foreach (var list in g)
+                {
+                    foreach (var e in list)
+                    {
+                        Console.Write(e);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+
+                var temp = new List<List<int>>();
+                var patharr = new List<int>();
+                for (var i = 0; i < g.Length; i++)
+                {
+                    for (var j = 0; j < g[i].Length; j++)
+                    {
+                        if (g[i][j] == startp )
+                        {
+                            temp.Add(new List<int>(g[i])); 
+                        }
+                    }
+                }
+                patharr.Add(startp);
+                foreach (var list in temp)
+                {
+                    Console.WriteLine(@"Potential way - start {0} dest {1}", list[0], list[1]);
+                    var startptr = list[1];
+                    foreach (var edgelist in g)
+                    {
+                        if (edgelist[0] == startptr)
+                        {   
+
+                            startptr = edgelist[0];
+                            patharr.Add(startptr);
+                            foreach (var subedges in g)
+                            {
+                                if (subedges[0] == startptr)
+                                {
+                          
+                                    patharr.Add(edgelist[1]);
+                                }
+                            }
+                        }
+                    }
+                }
+                Console.Write("Corrected path : ");
+                patharr.ToList().ForEach(Console.Write);
+                Out(patharr);
+            }
+        }
+    }
+}
---
+++ Npo-2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Npo-2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Npo-2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e8afa080-ef52-4cf9-99b5-c5a0c67578c6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
